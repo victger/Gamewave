a7c3be80b3644447c22f1bede15b666c345a89bd
Zakary Belkacem <108572199+Zak-Bel@users.noreply.github.com>
2023-02-26 00:28:53 +0100

    import elsaticsearch dans flask

--- hello.py
+++ hello.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, url_for, request, session, redirect
-from es_code import Video
-from elasticsearch_dsl import Search
+from flask import Flask, render_template
+from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
+es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    results = Video.search().execute()
-    return render_template('hello.html', results=results)
+    # Recherchez tous les documents dans l'index "yt_twitch"
+    result = es.search(index="yt_twitch", body={"query": {"match_all": {}}},size = 1500)
+    # Récupérez les documents et les stockez dans une liste
+    data = [hit['_source'] for hit in result['hits']['hits']]
+    return render_template('hello.html', data=data)
+
+
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
