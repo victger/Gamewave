23b43f4841e03534e46a3ddfb7eccd6c31c261c4
vgvictorgerard@gmail.com <vgvictorgerard@gmail.com>
2024-11-01 20:26:31 +0100

    Select fields to filter

--- app/elastic_utils.py
+++ app/elastic_utils.py
@@ -1,4 +1,42 @@
+from datetime import datetime, timedelta
+import re
 from elasticsearch.helpers import bulk
+
+def clean_views(views_str: str):
+    views_str= views_str.replace('k', '000').replace('M', '000000')
+    views_int= int(re.sub(r'[^\d]', '', views_str))
+    return views_int
+
+def clean_date(date_str: str):
+    today = datetime.today()
+    
+    if any(unit in date_str for unit in ["jour", "heure", "minute", "seconde"]):
+        days = int(re.search(r'(\d+)', date_str).group(1))
+        return (today - timedelta(days=days)).date()
+    elif "hier" in date_str:
+        return (today - timedelta(days=1)).date()
+    elif "semaine" in date_str:
+        weeks = int(re.search(r'(\d+)', date_str).group(1))
+        return (today - timedelta(weeks=weeks)).date()
+    elif "mois" in date_str:
+        months = int(re.search(r'(\d+)', date_str).group(1))
+        return (today - timedelta(days=months * 30)).date()
+    elif "an" in date_str:
+        years = int(re.search(r'(\d+)', date_str).group(1))
+        return (today - timedelta(days=years * 365)).date()
+    else:
+        try:
+            return datetime.strptime(date_str, '%Y-%m-%d').date()
+        except ValueError:
+            return None
+
+
+
+def process_data(data):
+    data["Views"]= data["Views"].apply(clean_views)
+    data["Date"]= data["Date"].apply(clean_date)
+    return data
+
 
 def generate_data(data):
     for docu in data:
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from chromedriver.ChromeDriver import launch_driver
 from twitch.Twitch import scrape_twitch
 from youtube.Youtube import scrape_youtube
 from app.elastic import es
-from app.elastic_utils import insert_data
+from app.elastic_utils import insert_data, process_data
 from app.flask_app import app
 
 # driver = launch_driver()
@@ -12,7 +12,9 @@ from app.flask_app import app
 
 # driver.quit()
 
-# data = data_youtube.to_dict('records')
-# insert_data(es, data)
+# process_data(data_youtube)
+
+# final_data = data_youtube.to_dict('records')
+# insert_data(es, final_data)
     
 app.run(debug=True)
